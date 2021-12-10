3893503ac071968dee7894e70c773b868cb6091c
Tao KAWARAI <35896206+taokawarai@users.noreply.github.com>
2021-12-10 11:13:57 +0900

    認証機能を実装 (#3)

--- app.py
+++ app.py
@@ -2,44 +2,39 @@ import os
 import tweepy
 from flask import Flask, session, redirect, render_template, request
 from os.path import join, dirname
+import urllib
 app = Flask(__name__)
 
 C_KEY = ''
 C_SECRET = ''
 CALLBACK_URL = 'http://0.0.0.0:5000'
 app.config['SECRET_KEY'] = os.urandom(24)
+auth = tweepy.OAuthHandler(C_KEY, C_SECRET, CALLBACK_URL)
 
 @app.route('/')
 def index():
+    oauth_token = request.args.get('oauth_token',default=' ',type=str)
+    oauth_verifier = request.args.get('oauth_verifier',default=' ',type=str)
+    auth.request_token['oauth_token_secret'] = oauth_verifier
+    if oauth_token != ' ':
+        try:
+            auth.get_access_token(oauth_verifier)
+        except Exception as ee:
+            return {}
+    
+        print("access token key:",auth.access_token)
+        print("access token secret:",auth.access_token_secret)
+        return '''
+            <p>ログイン成功</p>
+        '''
     return '''
         <p>ログインしてください</p>
+        <p><a href="{{ url_for('twitter_auth') }}">連携アプリ認証</a></p>
     '''
 
 @app.route('/twitter_auth', methods=['GET'])
 def twitter_auth():
-    # tweepy でアプリのOAuth認証を行う
-    auth = tweepy.OAuthHandler(C_KEY, C_SECRET, CALLBACK_URL)
     # 連携アプリ認証用の URL を取得
     redirect_url = auth.get_authorization_url()
-    # 認証後に必要な request_token を session に保存
-    session['request_token'] = auth.request_token
     # リダイレクト
     return redirect(redirect_url)
-
-def user_timeline():
-    # request_token と oauth_verifier のチェック
-    token = session.pop('request_token', None)
-    verifier = request.args.get('oauth_verifier')
-    if token is None or verifier is None:
-        return False  # 未認証ならFalseを返す
-
-    # tweepy でアプリのOAuth認証を行う
-    auth = tweepy.OAuthHandler(C_KEY, C_SECRET, CALLBACK_URL)
-
-    # Access token, Access token secret を取得．
-    auth.request_token = token
-    auth.get_access_token(verifier)
-
-    # tweepy で Twitter API にアクセス
-    api = tweepy.API(auth)
-    return api.user_timeline(count=100)
