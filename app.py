f2464d76e3c9ba0d729bb3b7a44f6559dd90f6f2
TAO-KAWARAI <taotako@gmail.com>
2021-12-11 04:17:04 +0900

    ツイート機能を追加、環境変数を外だし

--- app.py
+++ app.py
@@ -1,40 +1,35 @@
 import os
 import tweepy
 from flask import Flask, session, redirect, render_template, request
-from os.path import join, dirname
-import urllib
 app = Flask(__name__)
 
-C_KEY = ''
-C_SECRET = ''
-CALLBACK_URL = 'http://0.0.0.0:5000'
+CONSUMER_KEY = os.getenv('CONSUMER_KEY')
+CONSUMER_SECRET = os.getenv('CONSUMER_SECRET')
+CALLBACK_URL = os.getenv('CALLBACK_URL')
 app.config['SECRET_KEY'] = os.urandom(24)
-auth = tweepy.OAuthHandler(C_KEY, C_SECRET, CALLBACK_URL)
+auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET, CALLBACK_URL)
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    oauth_token = request.args.get('oauth_token',default=' ',type=str)
-    oauth_verifier = request.args.get('oauth_verifier',default=' ',type=str)
-    auth.request_token['oauth_token_secret'] = oauth_verifier
-    if oauth_token != ' ':
+    if request.method == 'GET':
+        oauth_token = request.args.get('oauth_token', default = None, type=str)
+        oauth_verifier = request.args.get('oauth_verifier',default = None, type=str)
+        if oauth_token == None:
+            return render_template("index.html", isAuthed = False)
         try:
+            auth.request_token['oauth_token_secret'] = oauth_verifier
             auth.get_access_token(oauth_verifier)
-        except Exception as ee:
-            return {}
-    
-        print("access token key:",auth.access_token)
-        print("access token secret:",auth.access_token_secret)
-        return '''
-            <p>ログイン成功</p>
-        '''
-    return '''
-        <p>ログインしてください</p>
-        <p><a href="{{ url_for('twitter_auth') }}">連携アプリ認証</a></p>
-    '''
+        except Exception as e:
+            return ''' <p>エラー</p> '''
+        return render_template("index.html", isAuthed = True)
+    elif request.method == 'POST':
+        auth.set_access_token(auth.access_token, auth.access_token_secret)
+        api = tweepy.API(auth)
+        msg = "水を " + str(request.form["msg"]) + " mL飲んだ`"
+        api.update_status(msg)
+        return render_template("index.html", isAuthed = True)
 
 @app.route('/twitter_auth', methods=['GET'])
 def twitter_auth():
-    # 連携アプリ認証用の URL を取得
     redirect_url = auth.get_authorization_url()
-    # リダイレクト
     return redirect(redirect_url)
